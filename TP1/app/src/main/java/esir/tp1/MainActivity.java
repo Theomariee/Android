a57cccade826ea8121b1468fc9c99a5214145e4b
Theomariee <theo.marie5@gmail.com>
2019-02-25 21:02:33 +0100

    First part of the TP1 app

---
+++ TP1/app/src/main/java/esir/tp1/MainActivity.java
@@ -0,0 +1,15 @@
+package esir.tp1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(this.getLocalClassName(), "Log info from MainActivity");
+    }
+}
